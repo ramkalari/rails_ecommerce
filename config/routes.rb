dd6feddd226f1423c3e95b68273777c7a247f231
Sriram Balasubramanian <sriram@juhomi.com>
2020-08-12 17:11:59 +0530

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :products
+  get 'say/hello'
+  get 'say/goodbye'
+  get 'welcome/index'
+  root 'welcome#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+
+
+#Rest compliant urls
